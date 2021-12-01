72797a1dc4c5e51bf93fd52c15e03ed7e24b0be1
wentingzoe <ywtzoe@gmail.com>
2021-12-01 15:39:04 -0500

    add name command to play

--- play.js
+++ play.js
@@ -1,24 +1,29 @@
-const net = require("net");
+const net = require('net');
 
+const connect = function() {
+	const conn = net.createConnection({
+		host: '165.227.47.243',
+		port: 50541
+	});
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
+	// interpret incoming data as text
+	conn.setEncoding('utf8');
+	conn.on('data', (data) => {
+		console.log('Server says: ', data);
+	});
+	conn.on('connect', () => {
+		console.log(`Server Says: `, 'successfully connected to the snake server');
+	});
+	conn.on('connect', () => {
+		conn.write('Name: YWT');
+	});
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
+	return conn;
 };
+
 module.exports = {
-  connect
+	connect
 };
 
-console.log("Connecting ...");
+console.log('Connecting ...');
 connect();
