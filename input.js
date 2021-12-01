13aabada811b055ce8eae85c6e01db8eb22ca14d
wentingzoe <ywtzoe@gmail.com>
2021-12-01 17:51:53 -0500

    WASD Movement

--- input.js
+++ input.js
@@ -1,17 +1,27 @@
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
+let connection;
+const setupInput = function(conn) {
+	connection = conn;
+	const stdin = process.stdin;
+	stdin.setRawMode(true);
+	stdin.setEncoding('utf8');
+	stdin.resume();
+	stdin.on('data', handleUserInput);
+	return stdin;
 };
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  } 
+const handleUserInput = function(key) {
+	// your code here
+	if (key === '\u0003') {
+		process.exit();
+	} else if (key === 'w') {
+		connection.write('Move: up');
+	} else if (key === 'a') {
+		connection.write('Move: left');
+	} else if (key === 's') {
+		connection.write('Move: down');
+	} else if (key === 'd') {
+		connection.write('Move: right');
+	} 
 };
 module.exports = {
-  setupInput
+	setupInput
 };
